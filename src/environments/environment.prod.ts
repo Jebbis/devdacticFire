0f093a6573b67b3965a16da4bde713677cdbfcbd
Jebbis <lassee96@hotmail.com>
2022-11-03 11:39:03 +0200

    .

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
+  firebase: {
+    projectId: 'labwork-4-f1239',
+    appId: '1:194019573581:web:b59964ce092fb5a4bbc104',
+    storageBucket: 'labwork-4-f1239.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyAOd4Vd-5E1VfDrREPGbCTZGEyzPzAU09E',
+    authDomain: 'labwork-4-f1239.firebaseapp.com',
+    messagingSenderId: '194019573581',
+    measurementId: 'G-R14SLVE1N5',
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  firebase: {
+    projectId: 'labwork-4-f1239',
+    appId: '1:194019573581:web:b59964ce092fb5a4bbc104',
+    storageBucket: 'labwork-4-f1239.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyAOd4Vd-5E1VfDrREPGbCTZGEyzPzAU09E',
+    authDomain: 'labwork-4-f1239.firebaseapp.com',
+    messagingSenderId: '194019573581',
+    measurementId: 'G-R14SLVE1N5',
+  },
+  production: false,
 };
 
 /*
